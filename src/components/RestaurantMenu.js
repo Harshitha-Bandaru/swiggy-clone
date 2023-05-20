33b2fbaf2c5d7e0d6d2a65c148dbe9234045c7ad
Harshitha-Bandaru <harshitha4547@gmail.com>
2023-05-21 03:33:22 +0530

    updated restaurant menu without categories

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -12,7 +12,19 @@ const RestaurantMenu = () => {
   // Destructure on the fly
   const { id } = useParams();
   // const [restaurant, setRestaurant] = useState({});
-  const restaurant = useRestaurant(id);
+  const [restaurantData, restaurantMenu] = useRestaurant(id);
+  const {
+    name,
+    areaName,
+    cuisines,
+    avgRating,
+    totalRatingsString,
+    costForTwoMessage,
+  } = restaurantData;
+  const lastMileTravelString = restaurantData?.sla?.lastMileTravelString;
+  const deliveryTime = restaurantData?.sla?.slaString;
+
+  console.log(restaurantMenu);
 
   const dispatch = useDispatch();
   const handleAddItem = () => {
@@ -24,28 +36,77 @@ const RestaurantMenu = () => {
   // if (!restaurant) {
   //   return <Shimmer />;
   // }
-  return !restaurant ? (
+  return !restaurantData ? (
     <Shimmer />
   ) : (
-    <div>
-      <h1>Restaurant id:{id}</h1>
-      <h2>{restaurant.name}</h2>
-      <img
-        className="restaurant-img w-52 h-1/4ff"
-        src={IMG_CDN_URL + restaurant.cloudinaryImageId}
-      ></img>
-      <h3>{restaurant.areaName}</h3>
-      {console.log(restaurant.cuisines)}
-      <h3>{restaurant.cuisines?.join(",")}</h3>
-      <h3>{restaurant.avgRating} stars</h3>
-      <h3>Cost for two:{restaurant.costForTwoMessage}</h3>
+    <div className="flex flex-col justify-center items-center ">
+      <div className="flex flex-row justify-items-center">
+        <div>
+          <h2 className="text-base font-bold ">{name}</h2>
+          <h3 className="text-gray-400">{cuisines?.join(",")}</h3>
+          <span className="text-gray-400">{areaName}, </span>
+          <span className="text-gray-400">{lastMileTravelString}</span>
+          <div>
+            <span className="text-gray-400 mt-2">
+              {lastMileTravelString + "|"}
+            </span>
+            <span className="text-gray-400 mt-2">
+              ₹40 delivery fee will apply
+            </span>
+          </div>
+          <div>
+            <span>{deliveryTime + ""}</span>
+            <span>{costForTwoMessage}</span>
+          </div>
+        </div>
+        <div className="border-solid">
+          <h3>{avgRating} stars</h3>
+        </div>
+      </div>
       <div>
-        <button
-          className="p-1 m-2 bg-green-400"
-          onClick={() => handleAddItem()}
-        >
-          Add
-        </button>
+        <h1>Menu</h1>
+        <ul>
+          {/* {regularCard?.map((c) => (
+            <>
+              {c?.card?.card?.categories?.map((i) => (
+                <>
+                  <li className="list-disc">{i?.title}</li>
+                  <>
+                    {i?.itemCards?.map((item) => (
+                      <li>{item?.card?.info?.name}</li>
+                    ))}
+                  </>
+                </>
+              ))}
+            </>
+          ))} */}
+          {restaurantMenu.map((i) => (
+            <div className="flex flex-row justify-between items-center p-1 m-2">
+              <div>
+                <li className="font-sans text-xl font-medium mt-1">
+                  {i?.menuItemName}
+                </li>
+                <li className=" font-normal text-lg mt-1">₹{i?.price / 100}</li>
+                <p className="text-base font-thin text-gray-400 mt-1">
+                  {i?.description}
+                </p>
+                <button
+                  className="p-1 m-2 bg-green-400"
+                  onClick={() => handleAddItem()}
+                >
+                  Add
+                </button>
+              </div>
+              <div>
+                <img
+                  className="h-32 w-32"
+                  src={IMG_CDN_URL + i?.menuItemImageId}
+                />
+              </div>
+              <hr />
+            </div>
+          ))}
+        </ul>
       </div>
     </div>
   );
--- src/utils/useRestaurant.js
+++ src/utils/useRestaurant.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 const useRestaurant = (id) => {
-  const [restaurant, setRestaurant] = useState(null);
+  const [restaurantData, setRestaurantData] = useState([]);
+  const [restaurantMenu, setRestaurantMenu] = useState([{}]);
   useEffect(() => {
     getRestaurantInfo();
   }, []);
@@ -9,11 +10,41 @@ const useRestaurant = (id) => {
       `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5912716&lng=73.73890899999999&restaurantId=${id}&submitAction=ENTER`
     );
     const json = await data.json();
-    // console.log(json.data.cards[0].card.card.info);
-    // console.table(json.data);
-    setRestaurant(json?.data?.cards[0]?.card?.card?.info);
+    setRestaurantData(json?.data?.cards[0]?.card?.card?.info);
+    console.log(json?.data?.cards[0]?.card?.card?.info);
+
+    const menuItems = [];
+    const regularCard =
+      json?.data?.cards?.[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+    regularCard?.map((c) => {
+      const category = c?.card?.card?.categories;
+      category?.map((i) => {
+        const itemCard = i?.itemCards;
+        // console.log(itemCard);
+        itemCard?.map((item) => {
+          const menuItem = item?.card?.info;
+          console.log(menuItem);
+          const {
+            id: menuItemId,
+            name: menuItemName,
+            price,
+            imageId: menuItemImageId,
+            description,
+          } = menuItem;
+          menuItems.push({
+            menuItemId,
+            menuItemName,
+            price,
+            menuItemImageId,
+            description,
+          });
+          // menuItems.push(menuItem);
+        });
+      });
+    });
+    setRestaurantMenu(menuItems);
   }
-  return restaurant;
+  return [restaurantData, restaurantMenu];
 };
 
 export default useRestaurant;
